86a965171008453b3ed2b95c5ae3e45eb9b1bd65
sasidharlolla3 <sasidharlolla3@gmail.com>
2024-10-02 17:19:47 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello from dashboard!");
-  });
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from server!");
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Sasidhar", lastname: "Lolla" });
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from Test");
+app.post("/user", (req, res) => {
+    res.send("DB saved successfully");
+  });
+
+  app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+  });
+
+app.use("/home", (req, res) => {
+  res.send("Hello from dashboard!");
 });
 
 app.listen(3000, () => {
